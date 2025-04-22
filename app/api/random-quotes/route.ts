d6e34b7069bd954e3a14dc14641bebef3b11466b
AnnMath <pap90@yahoo.com>
2025-04-22 13:58:38 +0200

    add json file and get random quote

--- app/api/random-quotes/route.ts
+++ app/api/random-quotes/route.ts
@@ -1,10 +1,9 @@
+import quotes from '@/data/quotes.json'
+
 export async function GET(request: Request) {
-  // For example, fetch data from your DB here
-  const users = [
-    { id: 1, name: 'Alice' },
-    { id: 2, name: 'Bob' },
-  ]
-  return new Response(JSON.stringify(users), {
+  const randomIndex = Math.floor(Math.random() * quotes.length)
+  const { id, quote, book, author } = quotes[randomIndex]
+  return new Response(JSON.stringify({ id, quote, book, author }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   })
